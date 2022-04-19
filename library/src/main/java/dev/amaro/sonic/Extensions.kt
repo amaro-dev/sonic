5e7a5f443693e5bc2945a4931a326266f83c608c
Rodrigo Amaro <rodrigo@amaro.dev>
2022-04-19 11:15:39 -0500

    Reviewing the Coroutine Scope approach

--- library/src/main/java/dev/amaro/sonic/Extensions.kt
+++ library/src/main/java/dev/amaro/sonic/Extensions.kt
@@ -1,21 +1,19 @@
 package dev.amaro.sonic
 
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
 
 inline fun <T> Flow<T>.collectOnDefault(crossinline action: suspend (value: T) -> Unit): Unit {
     collectOn(Dispatchers.Default, action)
 }
 
+
 inline fun <T> Flow<T>.collectOn(
     dispatcher: CoroutineDispatcher,
     crossinline action: suspend (value: T) -> Unit
-): Unit {
-    GlobalScope.launch(dispatcher) {
+) {
+    CoroutineScope(dispatcher).launch {
         collect { action(it) }
     }
 }

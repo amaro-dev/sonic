90664112493200b3c17147de8c8b570a3ca80fec
Amaro, Rodrigo <rodrigo@amaro.dev>
2025-12-12 12:54:17 -0300

    Moving the core components

--- sonic-core/src/main/java/dev/amaro/sonic/IMiddleware.kt
+++ sonic-core/src/main/java/dev/amaro/sonic/IMiddleware.kt
@@ -24,5 +24,5 @@ package dev.amaro.sonic
  * ```
  */
 interface IMiddleware<T> {
-   suspend fun process(action: IAction, state: T, processor: IProcessor<T>)
+    suspend fun process(action: IAction, state: T, processor: IProcessor<T>)
 }
